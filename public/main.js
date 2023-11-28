9eb016391f025d48a1e89fee9014b44882760d62
Richard Tran <rtran904@gmail.com>
2023-11-27 17:01:48 -0800

    Update web

--- public/main.js
+++ public/main.js
@@ -1,2 +1,14 @@
 console.log("Add your visualizations here!");
 console.log("hehe");
+
+window.addEventListener("scroll", () => {
+    let sideNav = document.querySelector(".sidenav");
+    let scrollHeight = document.documentElement.scrollTop;
+    let header = document.querySelector(".header");
+
+    if (scrollHeight > header.offsetHeight) {
+        sideNav.style.marginTop = `calc(${scrollHeight - header.offsetHeight}px + 3rem)`;
+    } else {
+        sideNav.style.marginTop = "3rem";
+    }
+});
