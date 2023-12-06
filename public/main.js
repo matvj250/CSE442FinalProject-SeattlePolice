8881cee884e02236933d53d0d9c3d3f90ee3c2d8
Richard Tran <RTran904@gmail.com>
2023-12-05 19:34:04 -0800

    Add Part3 table + link to observable notebook in credits

--- public/main.js
+++ public/main.js
@@ -1,5 +1,22 @@
 import {Runtime, Inspector} from "https://cdn.jsdelivr.net/npm/@observablehq/runtime@5/dist/runtime.js";
 import define from "https://api.observablehq.com/d/11c4b6df0988a406@1373.js?v=4";
+import crimeTableData from './data/crimes.json' assert {type: "json"};
+
+new Runtime().module(define, name => {
+    if (name === "part1AvgBar") return new Inspector(document.querySelector("#part1AvgBar"));
+    if (name === "part1MultiBar") return new Inspector(document.querySelector("#part1MultiBar"));
+    if (name === "viewof yearsmap") return new Inspector(document.querySelector("#part1MapYearSelect"));
+    if (name === "viewof priority") return new Inspector(document.querySelector("#part1MapPriorSelect"));
+    if (name === "map") return new Inspector(document.querySelector("#part1Map"));
+    if (name === "viewof respCityFactor") return new Inspector(document.querySelector("#part2RespCityFactorSelect"));
+    if (name === "respCityInfluences") return new Inspector(document.querySelector("#part2RespCityInfluences"));
+    if (name === "viewof group3") return new Inspector(document.querySelector("#part3DensityYearSelect"));
+    if (name === "popDensity") return new Inspector(document.querySelector("#part3Density"));
+    if (name === "viewof respDiffYear") return new Inspector(document.querySelector("#part3RespDiffYearSelect"));
+    if (name === "respDiffBar") return new Inspector(document.querySelector("#part3RespDiffBar"));
+    if (name === "viewof beatAspects") return new Inspector(document.querySelector("#part3beatAspectSelect"));
+    if (name === "demoHeatMap") return new Inspector(document.querySelector("#part3DemoHeatMap"));
+  });
 
 // Move side navigation on page scroll only if scrolled past page header
 window.addEventListener("scroll", () => {
@@ -22,18 +39,24 @@ for (let i = 0; i < cards.length; i++) {
     });
 }
 
-new Runtime().module(define, name => {
-  if (name === "part1AvgBar") return new Inspector(document.querySelector("#part1AvgBar"));
-  if (name === "part1MultiBar") return new Inspector(document.querySelector("#part1MultiBar"));
-  if (name === "viewof yearsmap") return new Inspector(document.querySelector("#part1MapYearSelect"));
-  if (name === "viewof priority") return new Inspector(document.querySelector("#part1MapPriorSelect"));
-  if (name === "map") return new Inspector(document.querySelector("#part1Map"));
-  if (name === "viewof respCityFactor") return new Inspector(document.querySelector("#part2RespCityFactorSelect"));
-  if (name === "respCityInfluences") return new Inspector(document.querySelector("#part2RespCityInfluences"));
-  if (name === "viewof group3") return new Inspector(document.querySelector("#part3DensityYearSelect"));
-  if (name === "popDensity") return new Inspector(document.querySelector("#part3Density"));
-  if (name === "viewof respDiffYear") return new Inspector(document.querySelector("#part3RespDiffYearSelect"));
-  if (name === "respDiffBar") return new Inspector(document.querySelector("#part3RespDiffBar"));
-  if (name === "viewof beatAspects") return new Inspector(document.querySelector("#part3beatAspectSelect"));
-  if (name === "demoHeatMap") return new Inspector(document.querySelector("#part3DemoHeatMap"));
+let table = document.querySelector("#part3CrimeTable tbody");
+crimeTableData.forEach((beat) => {
+    let row = document.createElement("tr");
+    let rowHeader = document.createElement("th");
+    rowHeader.scope = "row";
+    rowHeader.textContent = beat.beat;
+    row.appendChild(rowHeader);
+    for (let i = 2010; i <= 2020; i += 5) {
+        const crimes = beat["top-crimes-" + i];
+        let rowElement = document.createElement("td");
+        let crimesList =  document.createElement("ol");
+        for (const crime of crimes) {
+            let crimeElement = document.createElement("li");
+            crimeElement.textContent = crime.crime + " - " + crime.count;
+            crimesList.appendChild(crimeElement);
+        }
+        rowElement.appendChild(crimesList);
+        row.appendChild(rowElement);
+    }
+    table.appendChild(row);
 });
